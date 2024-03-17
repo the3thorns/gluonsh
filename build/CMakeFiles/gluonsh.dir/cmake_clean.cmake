46a506d295d98a30b15b9f051c54072dca06037a
the3thorns <the3thorns@gmail.com>
2024-03-17 17:01:45 +0100

    Added CMake support

---
+++ build/CMakeFiles/gluonsh.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gluonsh.dir/src/main.c.o"
+  "CMakeFiles/gluonsh.dir/src/main.c.o.d"
+  "gluonsh"
+  "gluonsh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gluonsh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/gluonsh.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gluonsh.dir/main.c.o"
+  "CMakeFiles/gluonsh.dir/main.c.o.d"
+  "gluonsh"
+  "gluonsh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gluonsh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
